ca43dad29748f6aef82f6669f56961ad2b0fbe2e
Alexei Domratchev <alexei.domratchev@rangle.io>
2021-05-20 09:04:18 -0400

    ds-button styling

--- libs/ds/src/lib/button/button.component.ts
+++ libs/ds/src/lib/button/button.component.ts
@@ -2,8 +2,7 @@ import {
   ChangeDetectionStrategy,
   Component,
   HostBinding,
-  Input,
-  ViewEncapsulation
+  Input
 } from '@angular/core';
 
 const selector = 'ds-button';
@@ -19,30 +18,9 @@ export type ButtonSize = typeof BUTTON_SIZES[number];
   selector: `button[${selector}]`,
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss'],
-  encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ButtonComponent {
-  // @HostBinding('class.ds-button--primary') get isPrimary(): boolean {
-  //   return this.variant === 'primary';
-  // }
-
-  // @HostBinding('class.ds-button--secondary') get isSecondary(): boolean {
-  //   return this.variant === 'secondary';
-  // }
-
-  // @HostBinding('class.ds-button--large') get isLarge(): boolean {
-  //   return this.size === 'large';
-  // }
-
-  // @HostBinding('class.ds-button--medium') get isMedium(): boolean {
-  //   return this.size === 'medium';
-  // }
-
-  // @HostBinding('class.ds-button--small') get isSmall(): boolean {
-  //   return this.size === 'small';
-  // }
-
   public variant: ButtonVariant = 'primary';
   public size: ButtonSize = 'medium';
 
@@ -61,7 +39,7 @@ export class ButtonComponent {
     this.size = BUTTON_SIZES.find(size => this._classNames.includes(size)) || 'medium';
   }
   public get classNames(): string {
-    return `${selector}--${this.variant} ${selector}--${this.size}`;
+    return `${selector} ${selector}--${this.variant} ${selector}--${this.size}`;
   }
   private _classNames!: string;
 }
